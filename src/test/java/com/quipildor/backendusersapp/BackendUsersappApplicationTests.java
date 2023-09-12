efb37206cac213e57d3d0698f6a1b09f217645bb
Quipildor-Claudio <claudioq00@gmail.com>
2023-09-12 10:30:42 -0300

    first commit

---
+++ src/test/java/com/quipildor/backendusersapp/BackendUsersappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quipildor.backendusersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUsersappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
